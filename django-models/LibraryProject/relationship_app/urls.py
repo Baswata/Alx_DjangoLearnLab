7b0d72078e923ddb107c63a3a18e47e5f6e530e3
Belinda Aswata <aswatabelinda@gmail.com>
2025-08-03 11:51:28 +0300

    correction done!

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView 
-from . import views
+from django.contrib.auth import views as auth_views
+from .views import list_books, LibraryDetailView, register_view
+
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path('register/', views.register_view, name='register'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
+
+    # Registration view (custom)
+    path('register/', register_view, name='register'),
+
+    # Login view (built-in, required by ALX)
+    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+
+    # Logout view (built-in, required by ALX)
+    path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
